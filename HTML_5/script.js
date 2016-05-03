87258d7ce727d729507772f4b38c3ec4999468b2
guitarShark <rosshenry170@gmail.com>
2016-05-02 23:00:42 -0600

    five two update2

--- HTML_5/script.js
+++ HTML_5/script.js
@@ -1,3 +1,13 @@
-function clickBait(){
-  alert(I have you now);
+function toggleExpand(id){
+  var entry = $("#" +id);
+  if(entry.css("display") == "none"){
+    entry.css("display", "block");
+    entry.parent().find(".collapseIcon").html("-");
+    entry.parent().find(".gameHeader").addClass("expanded");
+  }
+  else{
+    entry.css("display", "none");
+    entry.parent().find(".collapseIcon").html("+");
+    entry.parent().find(".gameHeader").removeClass("expanded");
+  }
 }
