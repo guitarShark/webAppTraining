06972ae55c808f261a0b0b111a85f3053bb5cda7
guitarShark <rosshenry170@gmail.com>
2016-05-12 20:46:14 -0600

    may12update

---
+++ HTML_6/script.js
@@ -0,0 +1,14 @@
+function toggleExpand(id){
+  var entry = $("#" +id);
+  if(entry.css("display") == "none"){
+    entry.css("display", "block");
+    entry.css("border", "none");
+    entry.parent().find(".collapseIcon").html("-");
+    entry.parent().find(".gameHeader").addClass("expanded");
+  }
+  else{
+    entry.css("display", "none");
+    entry.parent().find(".collapseIcon").html("+");
+    entry.parent().find(".gameHeader").removeClass("expanded");
+  }
+}
